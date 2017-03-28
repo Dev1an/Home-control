56783e4611d034cbf60a48ca06c0d59a0a63e6a6
Damiaan Dufaux <damiaan@dpro.be>
2017-03-28 23:51:25 +0200

    Add linux support

--- Radio/Package.swift
+++ Radio/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Radio",
     dependencies: [
-		.Package(url: "https://github.com/Dev1an/Cmpv.git", majorVersion: 0, minor: 1)
+		.Package(url: "https://github.com/Dev1an/Cmpv.git", majorVersion: 1)
 	]
 )
